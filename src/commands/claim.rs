633328d6ab3a85823f33c94624b3869f6ea05215
Charalampos Mitrodimas <charmitro@pm.me>
2022-03-08 03:46:26 +0200

    minor fixes & improvements

--- src/commands/claim.rs
+++ src/commands/claim.rs
@@ -1,8 +1,10 @@
+use std::env;
+
 use serenity::framework::standard::Args;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use sp_keyring::AccountKeyring;
+use sp_core::{sr25519::Pair, Pair as TraitPair};
 use subxt::sp_core::crypto::Ss58Codec;
 use subxt::sp_runtime::AccountId32;
 use subxt::sp_runtime::MultiAddress;
@@ -29,24 +31,40 @@ async fn claim(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
         .clone();
 
     let user_id = msg.author.id.to_string();
-
+    let phrase = env::var("PHRASE")?;
     let account_id = args.parse::<String>().unwrap();
-    match db::add::add_user(&*db_conn, user_id, account_id.clone()).await {
-        Err(err) => println!("{:?}", err),
-        Ok(_) => {
-            println!("passoed\n\n");
-            let signer = PairSigner::new(AccountKeyring::Alice.pair());
-            let dest = MultiAddress::from(AccountId32::from_string(&account_id).unwrap());
+    if let Err(err) = db::add::add_user(&*db_conn, user_id, account_id.clone()).await {
+        eprintln!("{:?}", err);
+        msg.author
+            .direct_message(&ctx.http, |dm| {
+                dm.content(format!(
+                    "Could not claim TCHER. Your Discord ID or account {} has already claimed.",
+                    &account_id.to_string()
+                ))
+            })
+            .await?;
+    } else {
+        let pair = Pair::from_string(&phrase.to_string(), None).unwrap();
+        let signer = PairSigner::<_, _, Pair>::new(pair);
+        let dest = MultiAddress::from(AccountId32::from_string(&account_id).unwrap());
 
-            let hash = api
-                .tx()
-                .balances()
-                .transfer_keep_alive(dest, 10_0000000000000) // existential deposit - @charmitro
-                .sign_and_submit(&signer)
-                .await?;
+        let hash = api
+            .tx()
+            .balances()
+            .transfer_keep_alive(dest, 10_0000000000000) // existential deposit - @charmitro
+            .sign_and_submit(&signer)
+            .await?;
 
-            println!("Balance transfer extrinscic submitted: {}", hash);
-        }
+        msg.author
+            .direct_message(&ctx.http, |dm| {
+                dm.content(format!(
+                    "Account {} claimed 100,0000 TCHER units.",
+                    &account_id.to_string()
+                ))
+            })
+            .await?;
+
+        println!("Balance transfer extrinscic submitted: {}", hash);
     }
 
     Ok(())
